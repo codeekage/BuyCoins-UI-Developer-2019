fdb73c42753bcf0eca25760b3c7194e74d8832cd
Linda-Ikechukwu <lindaedwin96@gmail.com>
2020-02-26 10:22:31 +0100

    changed code to use async await

--- index.js
+++ index.js
@@ -10,62 +10,111 @@ const tbody = document.querySelector('#coins-data');
 const prevButton = document.querySelector('#prev');
 const nextButton = document.querySelector('#next');
 
+const state = {};
 
-fetch('https://api.coinlore.com/api/tickers/')
-   .then(response => response.json())
-   .then(coins => {
-       let i = 0;
-       let arrLength = coins.data.length
-       console.log(arrLength);
-       let array = coins.data;
-       let coinsData = array.slice(0,10);
-       getCoins();
-
-       function nextCoins(){
-           i = i + 10;
-           i = i % array.length;
-           return array.slice(i, i+10);
-       }
-
-       function prevCoins(){
-           i = i - 10;
-           return array.slice(i, i+10);
+class Actions {
+  
+    constructor(coinsPerPage){
+        this.coinsPerPage = coinsPerPage;
+        
+    }
+    
+    async fetchCoins() {
+        try{
+            const response = await axios('https://api.coinlore.com/api/tickers/');
+            this.array = response.data.data;
+            console.log(this.array);
         }
-
-        function displayButtons(){
-            prevButton.style.visibility = i > 0 ?  "visible" : "hidden";
-            nextButton.style.visibility = i >= 90 ?  "hidden" : "visible";
+        catch(error){
+           console.log(error)
         }
+    }
+    nextCoins(){
+        let i =0;
+        i = i + this.coinsPerPage;
+        i = i % this.array;
+        return this.array.slice(i, i+coinsPerPage);
+    }
 
-       function getCoins(){
-            for(let coin of coinsData){
-                let tableRow = `<tr>
-                                <td>${coin.name}</td>
-                                <td>${coin.symbol}</td>
-                                <td>${coin.price_usd}</td>
-                                <td>${coin.tsupply} ${coin.symbol}</td>
-                                </tr>`
-                tbody.insertAdjacentHTML('beforeend', tableRow);     
-           }
-       }
+    prevCoins(){
+        let i = 0;
+        i = i - this.coinsPerPage;
+        return this.array.slice(i, i+coinsPerPage);
+    }
 
-        nextButton.addEventListener('click', function(e){
-           tbody.innerHTML = " ";
-           coinsData = nextCoins();
-           getCoins();
-           displayButtons()
-           
-        })
+    displayButtons(){
+         prevButton.style.visibility = i > 0 ?  "visible" : "hidden";
+         nextButton.style.visibility = i >= 90 ?  "hidden" : "visible";
+    }
+    
+    getCoins(coins){
+        for(let coin of coins){
+             let tableRow = `<tr>
+                         <td>${coin.name}</td>
+                         <td>${coin.symbol}</td>
+                         <td>${coin.price_usd}</td>
+                         <td>${coin.tsupply} ${coin.symbol}</td>
+                         </tr>`
+            tbody.insertAdjacentHTML('beforeend', tableRow);     
+         }
+    }
 
-        prevButton.addEventListener('click', function(e){
-            tbody.innerHTML = " ";
-            coinsData = prevCoins();
-            getCoins();
-            displayButtons()
-            
-        })
-       
-   }) 
-   .catch(error =>{
+    displayNewCoins(button){
+        let coins;
+        tbody.innerHTML = " ";
+        button === 'next' ? coins = this.nextCoins() : coins = this.prevCoins();
+        this.getCoins(coins);
+        this.displayButtons()
+    }
+    
+    init(){
+        let coinsData = this.array.slice(0,10);
+        this.getCoins(coinsData);
+    }
+}
+
+
+const newAction = async (direction) => {
+    state.coinData = new Actions(10);
+    try{
+      await state.coinData.fetchCoins();
+      //state.coinData.init();
+      state.coinData.displayNewCoins(direction);
+    }catch(error){
        console.log(error);
-   });
+    }
+    
+}
+newAction();
+
+/*const buttonClick = () =>{
+    state.coinsData.displayNewCoins
+    let coins;
+    tbody.innerHTML = " ";
+    button === 'next' ? coins = this.nextCoins() : coins = this.prevCoins();
+    this.getCoins(coins);
+    this.displayButtons()
+}*/
+
+const newAction2 = async (direction) => {
+    state.coinData = new Actions(10);
+    try{
+        await state.coinData.fetchCoins();
+        state.coinData.displayNewCoins(direction);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+
+ 
+nextButton.addEventListener('click', () => {
+    newAction('next');
+} );
+prevButton.addEventListener('click', () => {
+    newAction('prev');
+} );
+
+ 
+
+ 
